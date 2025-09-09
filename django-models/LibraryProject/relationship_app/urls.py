f0ad0be88445920284fc33796f74d057cf0a3362
Naomi <omageese402@gmail.com>
2025-09-09 16:06:27 +0100

    User Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view
+from . import views  # import views as a module
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register_view, name='register'),  # your custom registration view
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),  # use views.register for checker
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
