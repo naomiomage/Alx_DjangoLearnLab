16768230af3ef9334165f080cc093da5315d8ede
Naomi <omageese402@gmail.com>
2025-09-14 15:05:59 +0100

    question 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.csrf import csrf_protect
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 
 # ---------------------------
