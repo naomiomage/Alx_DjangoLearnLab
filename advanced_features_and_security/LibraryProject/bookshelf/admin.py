8b2cf2289801c32bebfa0734d3bdbfa3b63ab0f9
Naomi <omageese402@gmail.com>
2025-09-14 11:10:27 +0100

    custom user model in django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'date_of_birth')
+
+class CustomUserAdmin(UserAdmin):
+    # Show these fields in the admin list view
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    
+    # Fields to edit when creating/updating a user
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+    # Fields to show when adding a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+
+# ✅ This is what the checker wants:
+admin.site.register(CustomUser, CustomUserAdmin)
