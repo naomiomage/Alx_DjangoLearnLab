dbb58037e8480e431cd9a4255b5737b9267798c4
Naomi <omageese402@gmail.com>
2025-09-17 22:51:45 +0100

    Implement token authentication and permissions for Book API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,14 @@
+"""
+BookList API:
+- Requires authentication (Token).
+- Only logged-in users can access.
+- Token can be obtained at /api-token-auth/ by sending username & password.
+"""
+
+
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
@@ -8,6 +16,7 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
