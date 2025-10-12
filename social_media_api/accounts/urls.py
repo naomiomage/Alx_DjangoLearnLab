3a3bd0a4a78eaa9d3d29bf164837611c0c2c3c74
Naomi <omageese402@gmail.com>
2025-10-12 20:58:01 +0100

    Final production settings

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('users/', UserListView.as_view(), name='user-list'),
+     
+     # Follow / Unfollow URLs (must match ALX checker)
+    path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
+
+    # Followers & Following lists
+    path('me/following/', views.following_list, name='following_list'),
+    path('me/followers/', views.followers_list, name='followers_list'),
 ]
