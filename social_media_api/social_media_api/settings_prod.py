c32af60268d9876c682a2eb6cd3273578e3c0fd5
Naomi <omageese402@gmail.com>
2025-10-12 14:16:54 +0100

    Fix; update ALLOWED_HOSTS for Render deployment

--- social_media_api/social_media_api/settings_prod.py
+++ social_media_api/social_media_api/settings_prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,alx-djangolearnlab-ho2p.onrender.com').split(',')
 
 # Security settings
 SECURE_BROWSER_XSS_FILTER = True
