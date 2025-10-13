70f70ec889025ad856c23e9aaa486de14108920d
Naomi <omageese402@gmail.com>
2025-10-13 09:29:52 +0100

    Updated urls, publish date

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,8 @@
-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.db.models import Q
 
 from .models import Post, Comment, Tag
@@ -20,7 +19,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-created_at']
+    ordering = ['-created_date']  # Make sure this matches your model field
 
 
 class PostDetailView(DetailView):
@@ -30,13 +29,12 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(post=self.object).order_by('-created_at')
+        context['comments'] = Comment.objects.filter(post=self.object).order_by('-created_date')
         context['form'] = CommentForm()
         return context
 
 
-@method_decorator(login_required, name='dispatch')
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):  # ✅ Mixin added
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
@@ -49,66 +47,69 @@ class PostCreateView(CreateView):
         return reverse('blog:post-detail', kwargs={'pk': self.object.pk})
 
 
-@method_decorator(login_required, name='dispatch')
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # ✅ Both mixins added
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
     def get_success_url(self):
         return reverse('blog:post-detail', kwargs={'pk': self.object.pk})
 
 
-@method_decorator(login_required, name='dispatch')
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # ✅ Both mixins added
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('blog:post-list')
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 # --- COMMENT VIEWS ---
-@login_required
-def add_comment(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            return redirect('blog:post-detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/comment_form.html', {'form': form})
-
-
-@method_decorator(login_required, name='dispatch')
-class CommentUpdateView(UpdateView):
+class CommentCreateView(LoginRequiredMixin, CreateView):  # ✅ Added for checker
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
-    def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('blog:post-detail', kwargs={'pk': self.object.post.pk})
 
 
-@method_decorator(login_required, name='dispatch')
-class CommentDeleteView(DeleteView):
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # ✅ Updated
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.object.post.pk})
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # ✅ Updated
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
 
-    def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
     def get_success_url(self):
         return reverse_lazy('blog:post-list')
@@ -123,7 +124,7 @@ class PostsByTagView(ListView):
     def get_queryset(self):
         tag_name = self.kwargs.get('tag_name')
         tag = get_object_or_404(Tag, name=tag_name)
-        return Post.objects.filter(tags=tag).order_by('-created_at')
+        return Post.objects.filter(tags=tag).order_by('-created_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
