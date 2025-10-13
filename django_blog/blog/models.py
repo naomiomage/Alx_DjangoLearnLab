0bd56dcdab056d24715d2a16a0ba08b2ea825d8d
Naomi <omageese402@gmail.com>
2025-10-13 09:18:10 +0100

    Updated model, publish date

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,15 +10,18 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    tags = TaggableManager()  # ← replace your ManyToMany field with this
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
-
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
